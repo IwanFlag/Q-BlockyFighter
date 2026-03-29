64921e4f1cc4678926e310f6ebd68d3b27845579
thundersoft-blockfighter <2443150429@qq.com>
2026-03-29 09:20:43 +0800

    fix: MIME类型添加charset=utf-8修复Safari乱码

--- Server/server.js
+++ Server/server.js
@@ -284,10 +284,10 @@ function serveStatic(req, res) {
   
   const ext = path.extname(filePath);
   const mimeTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
+    '.html': 'text/html; charset=utf-8',
+    '.js': 'text/javascript; charset=utf-8',
+    '.css': 'text/css; charset=utf-8',
+    '.json': 'application/json; charset=utf-8',
     '.png': 'image/png',
     '.jpg': 'image/jpeg',
     '.svg': 'image/svg+xml',
